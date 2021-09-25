8619a5d8a2e6d77e3eb05309d84221f293043c29
yprawirocode <yusufprawiro14@gmail.com>
2021-09-25 15:45:40 +0700

    init commit

--- App.js
+++ App.js
@@ -1,45 +1,10 @@
-import React, { useState } from "react";
-import { Text, View } from "react-native";
-import * as Font from "expo-font";
-import AppLoading from "expo-app-loading";
-// import { useScreens } from 'react-native-screens'
-import { createStore, combineReducers } from "redux";
-import { Provider } from "react-redux";
-
-import MealsNavigator from "./navigation/MealsNavigator";
-import mealsReducer from "./store/reducers/meals";
-
-// useScreens();
-
-const rootReducer = combineReducers({
-  meals: mealsReducer,
-});
-
-const store = createStore(rootReducer);
-
-const fetchFonts = () => {
-  return Font.loadAsync({
-    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
-    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
-  });
-};
+import React from 'react';
+import { Text, View } from 'react-native';
 
 export default function App() {
-  const [fontLoaded, setFontLoaded] = useState(false);
-
-  if (!fontLoaded) {
-    return (
-      <AppLoading
-        startAsync={fetchFonts}
-        onFinish={() => setFontLoaded(true)}
-        onError={(err) => console.log(err)}
-      />
-    );
-  }
-
   return (
-    <Provider store={store}>
-      <MealsNavigator />
-    </Provider>
+    <View>
+      <Text>A new app!</Text>
+    </View>
   );
 }
