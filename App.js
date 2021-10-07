35a95dd6cfea865757b8002cb6aaa06c1260f0f9
yprawirocode <yusufprawiro14@gmail.com>
2021-10-07 14:14:18 +0700

    init commit

--- App.js
+++ App.js
@@ -1,54 +1,10 @@
-import React, { useState } from "react";
-import { createStore, combineReducers, applyMiddleware} from "redux";
-import { Provider } from "react-redux";
-import { LogBox } from "react-native";
-import AppLoading from "expo-app-loading";
-import * as Font from "expo-font";
-import ReduxThunk from 'redux-thunk';
-
-import productsReducer from "./store/reducers/products";
-import cartReducer from "./store/reducers/cart";
-import ordersReducer from './store/reducers/orders';
-import authReducer from "./store/reducers/auth";
-import NavigationContainer from './navigation/NavigationContainer'
-
-LogBox.ignoreLogs([
-  "Your project is accessing the following APIs from a deprecated global rather than a module import: Constants (expo-constants).",
-]);
-
-const rootReducer = combineReducers({
-  products: productsReducer,
-  cart: cartReducer,
-  orders: ordersReducer,
-  auth: authReducer
-});
-
-const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
-
-const fetchFonts = () => {
-  return Font.loadAsync({
-    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
-    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
-  });
-};
+import React from 'react';
+import { View } from 'react-native';
 
 export default function App() {
-  const [fontLoaded, setFontLoaded] = useState(false);
-
-  if (!fontLoaded) {
-    return (
-      <AppLoading
-        startAsync={fetchFonts}
-        onFinish={() => {
-          setFontLoaded(true);
-        }}
-        onError={(err) => console.log(err)}
-      />
-    );
-  }
   return (
-    <Provider store={store}>
-      <NavigationContainer />
-    </Provider>
+    <View>
+      
+    </View>
   );
 }
